8ea7513f089eef59832a0b2c27c2a66ff32a957b
antonyq <antonyq@ukr.net>
2016-11-01 14:03:26 +0200

    master

---
+++ MultithreadingAnd/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultithreadingAnd
+{
+	class Program
+	{
+		private static int x = 10, resultF, resultG;
+
+		public static void f()
+		{
+			resultF = (int) Math.Pow(x, 2);
+		}
+
+		public static void g()
+		{
+			resultG = (int) Math.Pow(x, 3);
+		}
+
+		static void Main(string[] args)
+		{
+			Console.Write("int x = ");
+			x = Console.Read();
+			Thread fThread = new Thread(f);
+			Thread gThread = new Thread(g);
+			fThread.Start();
+			gThread.Start();
+			Console.WriteLine(Convert.ToBoolean(resultF) && Convert.ToBoolean(resultG));
+			Console.Read();
+		}
+	}
+}
