c5e075a27fdeb28e01169f24e6396b22e4f32f36
antonyq <antonyq@ukr.net>
2016-11-08 18:43:46 +0200

    master

---
+++ MultithreadingAnd/DialogForm.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.ComponentModel;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace MultithreadingAnd
+{
+    public partial class DialogForm : Form
+    {
+        public Choice choice;
+
+        public DialogForm ()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click (object sender, EventArgs e)
+        {
+            choice = Choice.Continue;
+            this.Close();
+        }
+
+        private void button2_Click (object sender, EventArgs e)
+        {
+            choice = Choice.Cancel;
+            this.Close();
+        }
+
+        private void button3_Click (object sender, EventArgs e)
+        {
+            choice = Choice.ContinueEver;
+            this.Close();
+        }
+
+    }
+}
--- MultithreadingAnd/Program.cs
+++ MultithreadingAnd/Program.cs
@@ -1,36 +1,58 @@
 ﻿using System;
 using System.Threading;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace MultithreadingAnd
 {
+    public enum Choice {Continue, Cancel, ContinueEver};
+
 	class Program
 	{
-		private static int x = 10, resultF, resultG;
+		private static int resultF, resultG;
 
-		public static void f()
-		{
-			resultF = (int) Math.Pow(x, 2);
+		private static void f(object x)
+        {
+            resultF = (int)Math.Pow((int)x, 2);
 		}
 
-		public static void g()
+		private static void g(object x)
 		{
-			resultG = (int) Math.Pow(x, 3);
+			resultG = (int) Math.Pow((int)x, 3);
 		}
 
 		static void Main(string[] args)
 		{
 			Console.Write("int x = ");
-			x = Console.Read();
+			int x = Convert.ToInt32(Char.ConvertFromUtf32(Console.Read()));
 			Thread fThread = new Thread(f);
 			Thread gThread = new Thread(g);
-			fThread.Start();
-			gThread.Start();
-			Console.WriteLine(Convert.ToBoolean(resultF) && Convert.ToBoolean(resultG));
-			Console.Read();
+			fThread.Start(x);
+			gThread.Start(x);
+            while (fThread.ThreadState == System.Threading.ThreadState.Running || gThread.ThreadState == System.Threading.ThreadState.Running)
+            {
+                Thread.Sleep(2000);
+                DialogForm form = new DialogForm();
+                form.ShowDialog();
+                if (form.choice == Choice.ContinueEver)
+                {
+                    fThread.Join();
+                    gThread.Join(); // Klini logic 
+                }
+                else if (form.choice == Choice.Cancel)
+                {
+                    fThread.Abort();
+                    gThread.Abort();
+                }
+            }
+            if (fThread.ThreadState == System.Threading.ThreadState.Stopped && gThread.ThreadState == System.Threading.ThreadState.Stopped)
+            {
+                Console.WriteLine("f(x) && g(x) == " + (Convert.ToBoolean(resultF) && Convert.ToBoolean(resultG)).ToString());
+            }
+            else
+            {
+                Console.WriteLine("Calculations were not finished");
+            }
+            Console.ReadKey();
 		}
 	}
 }
